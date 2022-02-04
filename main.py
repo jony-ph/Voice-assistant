cbd71371839ed4797a6dd0a8cf273e2a75388286
Jony PH <jonathan_perez_hdz@hotmail.com>
2022-02-04 00:22:21 -0600

    main.py: Reconociminento de comandos
    Cambios:
    * Se creó archivo key_words
    * Se implementó función para reconocer el comando a ejecutar
    * Intento de wake word implementado

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-import src.Voice_client
+import src.Voice_Client as voice_client
+import src.engine as engine
+import key_words as key_words
+import sys
 
-vc = src.Voice_client.Voice_client()
+vc = voice_client.Voice_Client('eva')
+    
+def recognize_command(command):
+
+    function = ''
+
+    print(command)
+
+    for words in key_words.key_words:
+
+        for key in words:
+
+            if key in command:
+
+                function = key_words.key_words.index(words)
+                instruction = command.replace(key + ' ', '')
+
+
+if __name__ == '__main__':
+
+    vc.talk("Bienvenido. Activando el sistema")
+
+    while True:
+
+        command = vc.handler_activator()
+
+        if command != '':
+            recognize_command(command)
---
+++ src/key_words.py
@@ -0,0 +1,14 @@
+key_words = [
+
+    ['pon', 'reproduce', 'ponte'], # Música
+    ['busca'], # Búsquedas
+    ['abre', 'inicia', 'ejecuta', 'corre'], # Launcher
+    ['hora', 'qué hora es'], # Time
+    ['fecha', 'qué día es hoy', 'hoy es', 'a cómo estamos'], # Date
+    [
+        'hola', 'buenos días', 'buenas tardes', 'buenas noches', 'buen día', 'buena tarde', 'buena noche'
+        'que onda', 'quiobo', 'que hubo', 'que show', 'que rollo', 'qué pasó'  
+    ], # Saludos
+    ['salir', 'termina', 'finaliza', 'cierra', 'sal'], # Exit
+
+]
